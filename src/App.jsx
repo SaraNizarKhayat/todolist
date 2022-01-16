36b373be115e1a31e732fbf497a9c3716e55976b
Sara khayat <sara@interns.justechlb.tech>
2022-01-16 23:47:04 +0200

    Todo List build with REACT

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
